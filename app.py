b80b5ee7ed19a21274ea543fbb1955954555d580
Cameron Faith <cmrnfaith@gmail.com>
2022-04-22 14:47:29 -0600

    added Heroku Support

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask, abort
 from flask import jsonify
-from google.cloud import storage
-from google.oauth2 import service_account
 from PIL import Image
 import os
 import mimetypes
 
-GOOGLE_STORAGE_PROJECT = os.environ['GOOGLE_STORAGE_PROJECT']
-GOOGLE_STORAGE_BUCKET = os.environ['GOOGLE_STORAGE_BUCKET']
 
 app = Flask(__name__)
 
@@ -291,36 +287,7 @@ def _add_attribute(existing, attribute_name, options, token_id, display_type=Non
     existing.append(trait)
 
 
-def _compose_image(image_files, token_id, path='creature'):
-    composite = None
-    for image_file in image_files:
-        foreground = Image.open(image_file).convert('RGBA')
 
-        if composite:
-            composite = Image.alpha_composite(composite, foreground)
-        else:
-            composite = foreground
-
-    output_path = 'images/output/%s.png' % token_id
-    composite.save(output_path)
-
-    blob = _get_bucket().blob(f'{path}/{token_id}.png')
-    blob.upload_from_filename(filename=output_path)
-    return blob.public_url
-
-
-def _bucket_image(image_path, token_id, path='accessory'):
-    blob = _get_bucket().blob(f'{path}/{token_id}.png')
-    blob.upload_from_filename(filename=image_path)
-    return blob.public_url
-
-
-def _get_bucket():
-    credentials = service_account.Credentials.from_service_account_file('credentials/google-storage-credentials.json')
-    if credentials.requires_scopes:
-        credentials = credentials.with_scopes(['https://www.googleapis.com/auth/devstorage.read_write'])
-    client = storage.Client(project=GOOGLE_STORAGE_PROJECT, credentials=credentials)
-    return client.get_bucket(GOOGLE_STORAGE_BUCKET)
 
 
 ########################################################################
